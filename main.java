37c901d717116051dfb9e000f63c8a1d1dc6bc70
amicha19 <158494961+amicha19@users.noreply.github.com>
2024-02-12 16:23:53 -0500

    Update main.java

--- main.java
+++ main.java
@@ -3,11 +3,11 @@
 public class Main {
   
   public static void main(String[] args) {
-    int 1st_num = 1;
+    int first_num = 1;
     int last_num = 10;
-    int Sum = 0;
+    int sum = 0;
 
-    for(int i = 1st_num; i <= last_num; i++)
+    for(int i = first_num; i <= last_num; i++)
       sum = sum + i;
   
     System.out.println("Sum = " + sum);
